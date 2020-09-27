9727c3ec32c2e502857d7f004b8e9d7ab1ec3026
bianyingchun <2093213209@qq.com>
2020-09-27 19:46:08 +0800

    x

--- main.js
+++ main.js
@@ -1,12 +1,74 @@
 import { createElement, render, Component } from './toy-react'
-class MyComponent extends Component {
+
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render () {
-        return <div>
-            <h2>MyComponent</h2>
-            {this.children}
-        </div>
+        return (
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
+            </button>
+        );
     }
 }
 
+class Board extends Component {
+    renderSquare (i) {
+        return <Square />;
+    }
 
-render(<MyComponent class="c1" title="title"><div>abc</div></MyComponent>, document.body)
+    render () {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends Component {
+    render () {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+render(
+    <Game />,
+    document.getElementById('root')
+);
